36a9199307871b04dd319d1f200d8415eef7f14a
Rian Spies <spies2008@live.com>
2025-12-30 15:37:59 +0200

    "Project complete, Read me added"

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const generate = document.getElementById('generate');
 const randomP = document.getElementById('password');
+const copy = document.getElementById('copy');
 
 const generator = () =>{
     const char = 'abcdefghijklmnopqrstuvwxyz1234567890!@#$%&ABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -10,6 +11,20 @@ const generator = () =>{
         password += b  
     };
     randomP.value = password;
+};
+
+const clipboard = async() =>{
+    try{
+        if (randomP.value.length > 0){
+        await navigator.clipboard.writeText(randomP.value);
+        alert('Your password has been copied');
+        } else {
+            alert('Nothing to copy')
+        }   
+    } catch (err){
+        console.log(err)
+    }
 }
 
-generate.addEventListener('click', generator)
+generate.addEventListener('click', generator);
+copy.addEventListener('click', clipboard)
